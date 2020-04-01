37f181b7b88882e29fbe901e147c2c7b26126b48
KhosyAkmal <akmalkhosy@gmail.com>
2020-04-02 00:03:23 +0700

    create project and integration project to firebase

---
+++ src/config/firebase/index.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app';
+// import 'firebase/auth';
+// import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyCPEOmj5aFzAjvrNsqBQ5Mn_hq9J_ETqv4",
+    authDomain: "note-tugas-uts.firebaseapp.com",
+    databaseURL: "https://note-tugas-uts.firebaseio.com",
+    projectId: "note-tugas-uts",
+    storageBucket: "note-tugas-uts.appspot.com",
+    messagingSenderId: "911636960284",
+    appId: "1:911636960284:web:b1ac57fb9849995dd7bbd9",
+    measurementId: "G-LV88YS66KB"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
